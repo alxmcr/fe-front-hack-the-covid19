36ca40da020f561a72990049325b6ce59db69402
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-03-31 17:26:54 -0400

    - Comentario

--- src/repositories/AxiosRepository.js
+++ src/repositories/AxiosRepository.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import { HOST, API_VERSION } from "../config/backendServerConfig";
 
 const baseDomain = `https://${HOST}`;
-
 // The base URL
 const baseURL = `${baseDomain}/api/${API_VERSION}/`;
 
