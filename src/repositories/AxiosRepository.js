99fcf3199469ca5919d7a259374ee7b865209152
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-03-31 17:19:35 -0400

    * Cambio de Server heroku

--- src/repositories/AxiosRepository.js
+++ src/repositories/AxiosRepository.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-import { HOST, PORT, API_VERSION } from "../config/backendServerConfig";
+import { HOST, API_VERSION } from "../config/backendServerConfig";
 
-const baseDomain = `https://${HOST}:${PORT}`;
+const baseDomain = `https://${HOST}`;
 
 // The base URL
 const baseURL = `${baseDomain}/api/${API_VERSION}/`;
+console.log("baseURL", baseURL);
 
 export default axios.create({
   baseURL
