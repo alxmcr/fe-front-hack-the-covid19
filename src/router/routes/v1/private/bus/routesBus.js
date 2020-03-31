d0e6506dd4bbbc9f25881bc43787022fa8e00fe3
Alejandro M. Coca <alxmcr@outlook.com>
2020-03-31 02:41:20 -0400

    + Ruta Abordar Bus

--- src/router/routes/v1/private/bus/routesBus.js
+++ src/router/routes/v1/private/bus/routesBus.js
@@ -1,11 +1,17 @@
 import Buses from '../../../../../views/Privado/Bus/Buses.vue'
 import Bus from '../../../../../views/Privado/Bus/Bus.vue'
+import BusAbordar from '../../../../../views/Privado/Bus/BusAbordar.vue'
 
 export default [
   {
     path: '/buses',
     name: 'buses',
     component: Buses
+  },
+  {
+    path: '/buses/abordar',
+    name: 'buses-abordar',
+    component: BusAbordar
   },
   {
     path: '/buses/:bu_bus',
