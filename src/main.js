5078f29e8cf1516567f58f212a5890abd4276a8f
Alejandro M. Coca <alxmcr@outlook.com>
2020-03-30 21:31:28 -0400

    + Configuracion del Bootstrap y Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
@@ -8,6 +10,14 @@ import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
   router,
