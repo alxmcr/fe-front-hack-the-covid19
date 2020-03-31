ccf336c415c29d2add59b7f695e9465b77248da2
Alejandro M. Coca <alxmcr@outlook.com>
2020-03-30 22:37:18 -0400

    + Adicionar el componente RutaTableManager

--- src/views/Privado/Ruta/Rutas.vue
+++ src/views/Privado/Ruta/Rutas.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>Rutas</div>
+  <div>
+    <h1>Rutas</h1>
+    <RutaTableManager />
+  </div>
 </template>
 
 <script>
+import RutaTableManager from "@/components/Ruta/RutaTableManager";
+
 export default {
-    name: "Rutas"
-}
+  name: "Rutas",
+  components: { RutaTableManager }
+};
 </script>
 
 <style>
-
 </style>
