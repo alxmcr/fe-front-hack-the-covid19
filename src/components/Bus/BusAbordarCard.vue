ad8974b0582240477b531e61793d65e7fc7a510a
Alejandro M. Coca <alxmcr@outlook.com>
2020-03-31 03:13:46 -0400

    ! Trabajando con la tarjeta de abordaje

---
+++ src/components/Bus/BusAbordarCard.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-card color="primary" dark>
+    <v-card-title>Abordar un bus</v-card-title>
+    <v-card-subtitle>Busque el bus que desea abordar</v-card-subtitle>
+    <v-card-text>
+      <v-autocomplete
+        v-model="model"
+        :items="items"
+        :loading="isLoading"
+        :search-input.sync="search"
+        color="white"
+        hide-no-data
+        hide-selected
+        item-text="Description"
+        item-value="API"
+        label="Codigo del Bus"
+        placeholder="Start typing to Search"
+        prepend-icon="mdi-database-search"
+        return-object
+      ></v-autocomplete>
+    </v-card-text>
+    <v-divider></v-divider>
+    <v-expand-transition>
+      <v-list v-if="model" class="secondary">
+        <v-list-item v-for="(field, i) in fields" :key="i">
+          <v-list-item-content>
+            <v-list-item-title v-text="field.value"></v-list-item-title>
+            <v-list-item-subtitle v-text="field.key"></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-expand-transition>
+    <v-card-actions>
+      <v-btn :disabled="!model" color="secondary darken-3" @click="model = null">
+        Abordar
+        <v-icon right>mdi-check-circle</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn :disabled="!model" color="grey darken-3" @click="model = null">
+        Limpiar
+        <v-icon right>mdi-close-circle</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+// Repository Factory
+import { RepositoryFactory } from "../../repositories/RepositoryFactory";
+// Repositories
+const BusRepository = RepositoryFactory.get("bus");
+
+export default {
+  name: "BusAbordarCard",
+  data: () => ({
+    BusCodigoLimit: 3,
+    entries: [],
+    isLoading: false,
+    model: null,
+    search: null
+  }),
+
+  computed: {
+    fields() {
+      if (!this.model) return [];
+
+      return Object.keys(this.model).map(key => {
+        return {
+          key,
+          value: this.model[key] || "n/a"
+        };
+      });
+    },
+    items() {
+      return this.entries.map(entry => {
+        const BusCodigo =
+          entry.bu_codigo.length > this.BusCodigoLimit
+            ? entry.bu_codigo.slice(0, this.BusCodigoLimit) + "..."
+            : entry.bu_codigo;
+
+        return Object.assign({}, entry, { BusCodigo });
+      });
+    }
+  },
+
+  watch: {
+    search() {
+      // Items have already been loaded
+      if (this.items.length > 0) return;
+
+      // Items have already been requested
+      if (this.isLoading) return;
+
+      this.isLoading = true;
+
+      // Lazily load input items
+      BusRepository.get()
+        .then(response => {
+          this.entries = response.data.data;
+        })
+        .catch(error => {
+          console.error(error);
+        })
+        .finally(() => (this.isLoading = false));
+    }
+  }
+};
+</script>
