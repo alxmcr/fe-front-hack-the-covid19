ef37306d5a99346b9073efe4cd14fcc850faaf06
Alejandro M. Coca <alxmcr@outlook.com>
2020-03-30 22:18:48 -0400

    + Nuevo enlaces en la barra de navegacion

--- src/components/Comunes/AppNavBar.vue
+++ src/components/Comunes/AppNavBar.vue
@@ -1,30 +1,30 @@
 <template>
   <!-- <v-app-bar app color="primary" dark> -->
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <b-navbar toggleable="lg" type="dark" class="primary">
+      <b-navbar-brand href="#">La Paz Te Cuida</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          <b-nav-item to="/">Inicio</b-nav-item>
+          <b-nav-item to="/about">Nuestro servicio</b-nav-item>
+          <b-nav-item to="/rutas">Rutas</b-nav-item>
+          <b-nav-item to="/buses">Buses</b-nav-item>
+          <b-nav-item to="/viajes">Viajes</b-nav-item>
+          <b-nav-item to="/usuarios">Usuarios</b-nav-item>
+          <b-nav-item to="/usuarios/enfermos">Confirmados con COVID-19</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form>
-
-          <b-nav-item-dropdown text="Lang" right>
+          <!-- <b-nav-item-dropdown text="Idioma" right>
             <b-dropdown-item href="#">EN</b-dropdown-item>
             <b-dropdown-item href="#">ES</b-dropdown-item>
             <b-dropdown-item href="#">RU</b-dropdown-item>
             <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
+          </b-nav-item-dropdown>-->
 
           <b-nav-item-dropdown right>
             <!-- Using 'button-content' slot -->
