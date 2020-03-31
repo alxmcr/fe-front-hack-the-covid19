0d833d625e1435d06b8e863d7ea44020e037f85b
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-03-31 15:58:02 -0400

    ! trabajando con la barra de navegacion

--- src/components/Comunes/AppNavBar.vue
+++ src/components/Comunes/AppNavBar.vue
@@ -63,24 +63,29 @@
           </v-menu>
         </b-navbar-nav>
 
+        <b-nav>
+          <v-btn color="success" dark to="/register">Registrarse</v-btn>
+          <v-btn color="info" dark to="/login">Ingresar</v-btn>
+        </b-nav>
+
         <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <!-- <b-nav-item-dropdown text="Idioma" right>
+        <!-- <b-navbar-nav class="ml-auto"> -->
+        <!-- <b-nav-item-dropdown text="Idioma" right>
             <b-dropdown-item href="#">EN</b-dropdown-item>
             <b-dropdown-item href="#">ES</b-dropdown-item>
             <b-dropdown-item href="#">RU</b-dropdown-item>
             <b-dropdown-item href="#">FA</b-dropdown-item>
           </b-nav-item-dropdown>-->
 
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template v-slot:button-content>
+        <!-- <b-nav-item-dropdown right> -->
+        <!-- Using 'button-content' slot -->
+        <!-- <template v-slot:button-content>
               <em>User</em>
             </template>
             <b-dropdown-item href="#">Profile</b-dropdown-item>
             <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
+          </b-nav-item-dropdown> -->
+        <!-- </b-navbar-nav> -->
       </b-collapse>
     </b-navbar>
   </div>
