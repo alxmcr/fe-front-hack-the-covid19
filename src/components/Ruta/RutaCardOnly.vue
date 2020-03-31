9ab02710577e6b2b7354e2b2ea7693da61464eea
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-03-31 13:30:53 -0400

    + Tarjeta Ruta de Lectura

---
+++ src/components/Ruta/RutaCardOnly.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card>
+    <v-card-title>Ruta #{{ ruta.ru_ruta }}</v-card-title>
+    <v-card-text>
+      <v-list class="secondary lighten-3">
+        <v-list-item>
+          <v-list-item-content>
+            <b>Codigo:</b>
+            <span>{{ ruta.ru_codigo }}</span>
+          </v-list-item-content>
+          <v-list-item-content>
+            <b>Lugar Partida:</b>
+            <span>{{ ruta.ru_lugar_partida }}</span>
+          </v-list-item-content>
+          <v-list-item-content>
+            <b>Lugar Destino:</b>
+            <span>{{ ruta.ru_lugar_destino }}</span>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["ruta"],
+  name: "RutaCardOnly"
+};
+</script>
+
+<style></style>
