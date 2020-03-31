b5a0e78eb9b6a91ad6f9621502ec46c2f1929c4e
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-03-31 12:14:52 -0400

    + Tarjeta de Viaje a Abordar

---
+++ src/components/Viaje/ViajeAbordarCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card>
+    <v-card-title>Viaje: {{ viaje.vi_codigo }}</v-card-title>
+    <v-card-text>
+      <v-list class="secondary lighten-3">
+        <v-list-item>
+          <v-list-item-content>
+            <b>Fecha Viaje:</b>
+            <span>{{ viaje.vi_fecha_viaje }}</span>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn :disabled="!viaje" color="green darken-3" @click="viaje != null">
+        Abordar
+        <v-icon right>mdi-check-circle</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["viaje"],
+  name: "ViajeAbordarCard"
+};
+</script>
+
+<style></style>
