372807ed242cd145d1a651e545d285fd4686ad42
Alejandro M. Coca <alxmcr@outlook.com>
2020-03-31 02:06:00 -0400

    + Tabla de Usuarios

--- src/views/Privado/Usuario/Usuarios.vue
+++ src/views/Privado/Usuario/Usuarios.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>Usuarios</div>
+  <div>
+    <h1>Usuarios</h1>
+    <UsuarioTableManager />
+  </div>
 </template>
 
 <script>
+import UsuarioTableManager from "@/components/Usuario/UsuarioTableManager";
+
 export default {
-    name: "Usuarios"
-}
+  name: "Usuarios",
+  components: { UsuarioTableManager }
+};
 </script>
 
 <style>
-
 </style>
