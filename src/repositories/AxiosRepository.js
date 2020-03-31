b8ca9aaa483d24ef06bf694521507abe26e2a85b
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-03-31 17:22:13 -0400

    * Correcciones

--- src/repositories/AxiosRepository.js
+++ src/repositories/AxiosRepository.js
@@ -6,7 +6,6 @@ const baseDomain = `https://${HOST}`;
 
 // The base URL
 const baseURL = `${baseDomain}/api/${API_VERSION}/`;
-console.log("baseURL", baseURL);
 
 export default axios.create({
   baseURL
