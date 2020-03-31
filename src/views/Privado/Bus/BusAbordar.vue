c6025f24bd95ca6813c1491283c854827f36bebc
Alejandro M. Coca <alxmcr@outlook.com>
2020-03-31 02:41:44 -0400

    + Vista para Abordar un Bus

---
+++ src/views/Privado/Bus/BusAbordar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      Aqui abordars un bus
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BusAbordar"
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/Privado/Viaje/ViajeIniciar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Viaje Iniciar</div>
+</template>
+
+<script>
+export default {
+  name: "ViajeIniciar"
+};
+</script>
+
+<style></style>
