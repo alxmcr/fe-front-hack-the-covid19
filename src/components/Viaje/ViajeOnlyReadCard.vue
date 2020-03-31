5f809fc11457c005d9d6e4a5ecf0cb1b62d1cddb
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-03-31 12:09:05 -0400

    + Tarjeta para visualizar un viaje

---
+++ src/components/Viaje/ViajeOnlyReadCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card>
+    <v-card-title>Viaje: {{ viaje.vi_codigo }}</v-card-title>
+    <v-card-text>
+      <v-list class="secondary lighten-3">
+        <v-list-item>
+          <v-list-item-content>
+            <b>Fecha Viaje:</b>
+            <span>{{ viaje.vi_fecha_viaje }}</span>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["viaje"],
+  name: "ViajeOnlyRead"
+};
+</script>
+
+<style></style>
