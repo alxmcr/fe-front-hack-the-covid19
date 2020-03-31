a82f10745464fa5ec667000d02616282651c2ea0
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-03-31 10:58:56 -0400

    ! Trabajando con la version de ejemplo de auto-complete

--- src/components/Bus/BusAbordarCard.vue
+++ src/components/Bus/BusAbordarCard.vue
@@ -1,7 +1,18 @@
 <template>
-  <v-card color="primary" dark>
-    <v-card-title>Abordar un bus</v-card-title>
-    <v-card-subtitle>Busque el bus que desea abordar</v-card-subtitle>
+  <v-card color="red lighten-2" dark>
+    <v-card-title class="headline red lighten-3">
+      Search for Public APIs
+    </v-card-title>
+    <v-card-text>
+      Explore hundreds of free API's ready for consumption! For more information
+      visit
+      <a
+        class="grey--text text--lighten-3"
+        href="https://github.com/toddmotto/public-apis"
+        target="_blank"
+        >the Github repository</a
+      >.
+    </v-card-text>
     <v-card-text>
       <v-autocomplete
         v-model="model"
@@ -11,54 +22,38 @@
         color="white"
         hide-no-data
         hide-selected
-        item-text="Codigo"
+        item-text="Description"
         item-value="API"
-        label="Codigo del Bus"
-        placeholder="Inicia escribiendo el codigo"
+        label="Public APIs"
+        placeholder="Start typing to Search"
         prepend-icon="mdi-database-search"
         return-object
       ></v-autocomplete>
     </v-card-text>
     <v-divider></v-divider>
     <v-expand-transition>
-      <v-list v-if="model" class="secondary">
+      <v-list v-if="model" class="red lighten-3">
         <v-list-item v-for="(field, i) in fields" :key="i">
           <v-list-item-content>
-            <v-list-item-title v-text="field.ru_ruta"></v-list-item-title>
-            <v-list-item-subtitle
-              v-text="field.bu_codigo"
-            ></v-list-item-subtitle>
+            <v-list-item-title v-text="field.value"></v-list-item-title>
+            <v-list-item-subtitle v-text="field.key"></v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-expand-transition>
     <v-card-actions>
-      <v-btn
-        :disabled="!model"
-        color="secondary darken-3"
-        @click="model = null"
-      >
-        Abordar
-        <v-icon right>mdi-check-circle</v-icon>
-      </v-btn>
       <v-spacer></v-spacer>
       <v-btn :disabled="!model" color="grey darken-3" @click="model = null">
-        Limpiar
+        Clear
         <v-icon right>mdi-close-circle</v-icon>
       </v-btn>
     </v-card-actions>
   </v-card>
 </template>
 <script>
-// Repository Factory
-import { RepositoryFactory } from "../../repositories/RepositoryFactory";
-// Repositories
-const BusRepository = RepositoryFactory.get("bus");
-
 export default {
-  name: "BusAbordarCard",
   data: () => ({
-    BusCodigoLimit: 5,
+    descriptionLimit: 60,
     entries: [],
     isLoading: false,
     model: null,
@@ -77,37 +72,19 @@ export default {
       });
     },
     items() {
-      //console.log("entries", this.entries);
-      console.log("search", this.search);
+      return this.entries.map(entry => {
+        const Description =
+          entry.Description.length > this.descriptionLimit
+            ? entry.Description.slice(0, this.descriptionLimit) + "..."
+            : entry.Description;
 
-      let entriesMap = this.entries.map(entry => {
-        // console.log("entry", entry);
-
-        const { bu_codigo } = entry;
-        // console.log("bu_codigo", bu_codigo);
-
-        const BusCodigo =
-          bu_codigo.length > this.BusCodigoLimit
-            ? bu_codigo.slice(0, this.BusCodigoLimit) + "..."
-            : bu_codigo;
-
-        // console.log("BusCodigo", BusCodigo);
-
-        const result = Object.assign({}, entry, { BusCodigo });
-        console.log("result", result);
-
-        return result;
+        return Object.assign({}, entry, { Description });
       });
-      console.log("entriesMap", entriesMap);
-
-      return entriesMap;
     }
   },
 
   watch: {
-    search(value) {
-      console.log(value);
-      console.log(this.items);
+    search() {
       // Items have already been loaded
       if (this.items.length > 0) return;
 
@@ -117,12 +94,15 @@ export default {
       this.isLoading = true;
 
       // Lazily load input items
-      BusRepository.get()
-        .then(response => {
-          this.entries = response.data.data;
+      fetch("https://api.publicapis.org/entries")
+        .then(res => res.json())
+        .then(res => {
+          const { count, entries } = res;
+          this.count = count;
+          this.entries = entries;
         })
-        .catch(error => {
-          console.error(error);
+        .catch(err => {
+          console.log(err);
         })
         .finally(() => (this.isLoading = false));
     }
